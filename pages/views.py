83b7f2f461ac70325bf91d5479db5e4c737f9734
Anton <anton@mail.ru>
2019-09-16 14:16:23 +0300

    I've edited views

--- pages/views.py
+++ pages/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 
 
 def homePageView(reques):
-    return HttpResponse("Hello World I've done some edits!!")
+    return HttpResponse("Hello World I try to test branch ! ! !! ")
  
